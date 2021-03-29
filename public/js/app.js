b6d0dc7a8d26ba01c3fcc26b40f7328fe5f995f9
Manmeet <mimist2761@gmail.com>
2021-03-29 15:07:25 +0530

    Second

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
      const location = search.value
    
-   fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+   fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
            message1.textContent=data.error
--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const geocode = require('./utils/geocode')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewpaths = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
@@ -113,6 +115,6 @@ app.get('*',(req,res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port )
 })
